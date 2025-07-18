9f32c5c12706cf0d1a7764890b0f89118222c2f0
Mattia Manzati <manzati.mattia@gmail.com>
2025-07-18 09:34:19 +0200

    Add span attributes in current span view (#34)

---
+++ .changeset/lemon-towns-trade.md
@@ -0,0 +1,5 @@
+---
+"effect-vscode": patch
+---
+
+Add span attributes in current span view
--- src/DebugSpanStackProvider.ts
+++ src/DebugSpanStackProvider.ts
@@ -1,15 +1,29 @@
-import type * as Array from "effect/Array"
+import * as Array from "effect/Array"
 import * as Effect from "effect/Effect"
 import * as Option from "effect/Option"
 import * as Stream from "effect/Stream"
 import * as SubscriptionRef from "effect/SubscriptionRef"
 import * as vscode from "vscode"
+import type * as DebugChannel from "./DebugChannel"
 import * as Debug from "./DebugEnv"
 import { registerCommand, revealFile, TreeDataProvider, treeDataProvider } from "./VsCode"
 
-class TreeNode {
-  constructor(readonly entry: Debug.SpanStackEntry) {}
+class SpanNode {
+  readonly _tag = "SpanNode"
+  constructor(readonly span: Debug.SpanStackEntry) {}
 }
+
+class AttributeNode {
+  readonly _tag = "AttributeNode"
+  constructor(readonly name: string, readonly variable: DebugChannel.VariableReference) {}
+}
+
+class VariableNode {
+  readonly _tag = "VariableNode"
+  constructor(readonly variable: DebugChannel.VariableReference) {}
+}
+
+type TreeNode = SpanNode | AttributeNode | VariableNode
 
 export const DebugSpanStackProviderLive = treeDataProvider<TreeNode>("effect-debug-span-stack")(
   (refresh) =>
@@ -18,10 +32,10 @@ export const DebugSpanStackProviderLive = treeDataProvider<TreeNode>("effect-deb
       let nodes: Array<TreeNode> = []
 
       yield* registerCommand("effect.revealSpanLocation", (node: TreeNode) => {
-        if (node && node.entry.path) {
+        if (node && node._tag === "SpanNode" && node.span.path) {
           return revealFile(
-            node.entry.path,
-            new vscode.Range(node.entry.line, node.entry.column, node.entry.line, node.entry.column)
+            node.span.path,
+            new vscode.Range(node.span.line, node.span.column, node.span.line, node.span.column)
           )
         }
         return Effect.void
@@ -34,7 +48,7 @@ export const DebugSpanStackProviderLive = treeDataProvider<TreeNode>("effect-deb
         } else {
           const session = sessionOption.value
           const pairs = yield* _(session.currentSpanStack)
-          nodes = pairs.map((_) => new TreeNode(_))
+          nodes = pairs.map((_) => new SpanNode(_))
         }
         yield* _(refresh(Option.none()))
       })
@@ -63,7 +77,7 @@ export const DebugSpanStackProviderLive = treeDataProvider<TreeNode>("effect-deb
       return TreeDataProvider<TreeNode>({
         children: Option.match({
           onNone: () => Effect.succeedSome(nodes),
-          onSome: () => Effect.succeedNone
+          onSome: (node) => children(node)
         }),
         treeItem: (node) => Effect.succeed(treeItem(node))
       })
@@ -72,18 +86,73 @@ export const DebugSpanStackProviderLive = treeDataProvider<TreeNode>("effect-deb
 
 // === helpers ===
 
+const children = (node: TreeNode) => {
+  switch (node._tag) {
+    case "SpanNode": {
+      if (node.span.attributes.length > 0) {
+        return Effect.succeedSome(
+          Array.map(node.span.attributes, ([name, variable]) => new AttributeNode(name, variable))
+        )
+      }
+      return Effect.succeedNone
+    }
+    case "AttributeNode": {
+      return node.variable.children.pipe(
+        Effect.map(Array.map((_) => new VariableNode(_))),
+        Effect.orElseSucceed(() => []),
+        Effect.asSome
+      )
+    }
+    case "VariableNode": {
+      return node.variable.children.pipe(
+        Effect.map(Array.map((_) => new VariableNode(_))),
+        Effect.orElseSucceed(() => []),
+        Effect.asSome
+      )
+    }
+  }
+}
+
 const treeItem = (node: TreeNode): vscode.TreeItem => {
-  const item = new vscode.TreeItem(
-    node.entry.name,
-    vscode.TreeItemCollapsibleState.None
-  )
-  if (node.entry.path) {
-    item.description = vscode.workspace.asRelativePath(node.entry.path) + ":" + node.entry.line + ":" +
-      node.entry.column
+  switch (node._tag) {
+    case "SpanNode": {
+      const item = new vscode.TreeItem(
+        node.span.name,
+        node.span.attributes.length > 0
+          ? vscode.TreeItemCollapsibleState.Collapsed
+          : vscode.TreeItemCollapsibleState.None
+      )
+      if (node.span.path) {
+        item.description = vscode.workspace.asRelativePath(node.span.path) + ":" + node.span.line + ":" +
+          node.span.column
+      }
+      if (node.span.stackIndex >= 1) {
+        item.iconPath = new vscode.ThemeIcon("indent")
+      }
+      item.tooltip = node.span.spanId
+      return item
+    }
+    case "AttributeNode": {
+      const item = new vscode.TreeItem(
+        node.name + ":",
+        node.variable.isContainer
+          ? vscode.TreeItemCollapsibleState.Collapsed
+          : vscode.TreeItemCollapsibleState.None
+      )
+      item.description = node.variable.value
+      item.tooltip = node.variable.value
+      return item
+    }
+    case "VariableNode": {
+      const item = new vscode.TreeItem(
+        node.variable.name + ":",
+        node.variable.isContainer
+          ? vscode.TreeItemCollapsibleState.Collapsed
+          : vscode.TreeItemCollapsibleState.None
+      )
+      item.description = node.variable.value
+      item.tooltip = node.variable.value
+      return item
+    }
   }
-  if (node.entry.stackIndex >= 1) {
-    item.iconPath = new vscode.ThemeIcon("indent")
-  }
-  item.tooltip = node.entry.spanId
-  return item
 }
