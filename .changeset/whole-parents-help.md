8ecf88178c6983d3bcd0514a8e6eed6e830d955c
Mattia Manzati <mattia.manzati@gmail.com>
2025-09-12 16:03:28 +0200

    Adds a mechanism to preload the effect's debugger instrumentation before node begins executing userlands scripts.

---
+++ .changeset/whole-parents-help.md
@@ -0,0 +1,5 @@
+---
+"effect-vscode": patch
+---
+
+Adds a mechanism to preload the effect's debugger instrumentation before node begins executing userlands scripts.
---
+++ src/InjectNodeOptionsInstrumentationProvider.ts
@@ -0,0 +1,52 @@
+import * as Effect from "effect/Effect"
+import * as Layer from "effect/Layer"
+import * as Runtime from "effect/Runtime"
+import * as vscode from "vscode"
+import { configWithDefault, VsCodeContext } from "./VsCode"
+
+export const InjectNodeOptionsInstrumentationLive = Effect.gen(function*() {
+  const extension = yield* VsCodeContext
+
+  // gets the config if it shuold be injected or not
+  const injectNodeOptions = yield* configWithDefault("effect.instrumentation", "injectNodeOptions", false)
+  const injectDebugConfigurations = yield* configWithDefault("effect.instrumentation", "injectDebugConfigurations", [
+    "node",
+    "node-terminal",
+    "pwa-node"
+  ])
+  const runtime = yield* Effect.runtime<never>()
+
+  yield* Effect.acquireRelease(
+    Effect.sync(() =>
+      vscode.debug.registerDebugConfigurationProvider("*", {
+        resolveDebugConfiguration(_folder, config, _token) {
+          // if disabled (default) then do nothing
+          const shouldInjectBool = Runtime.runSync(runtime, injectNodeOptions.get)
+          if (!shouldInjectBool) return config
+
+          // abort immediately if the token is cancelled
+          if (_token?.isCancellationRequested) return config
+
+          // if not supported, then do nothing
+          const debugConfigurations = Runtime.runSync(runtime, injectDebugConfigurations.get)
+          if (
+            debugConfigurations.map((_) => String(_).toLowerCase()).indexOf(config.type.toLowerCase()) === -1
+          ) return config
+
+          // if enabled, then inject the instrumentation in NODE_OPTIONS
+          const configEnv = config.env || {}
+          const previousNodeOptions = configEnv.NODE_OPTIONS || ""
+          const instrumentationPath = JSON.stringify(extension.extensionPath + "/out/instrumentation.global.js")
+          return {
+            ...config,
+            env: {
+              ...configEnv,
+              "NODE_OPTIONS": `--require ${instrumentationPath} ${previousNodeOptions}`
+            }
+          }
+        }
+      })
+    ),
+    (disposer) => Effect.sync(() => disposer.dispose())
+  )
+}).pipe(Layer.scopedDiscard)
